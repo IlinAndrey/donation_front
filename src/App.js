01560d2f46c4bc5da38c2833dfd5083bf47ca86a
Andrey <frayz2507@gmail.com>
2023-11-17 14:52:40 +0300

    navbar and sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import { useState, useRef } from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import SidebarToggle from "./components/SidebarToggle";
+import { useClickOutside } from "./functions/useClickOutside";
+import SomePage from "./pages/SomePage";
+
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  function toggleSidebarOpen(callbackSidebarOpen) {
+    setSidebarOpen((callbackSidebarOpen) => !callbackSidebarOpen);
+  }
+
+  const menuRef = useRef(null);
+  useClickOutside(menuRef, () => setSidebarOpen(false));
+
   return (
-    <></>
+    <>
+      <div className="bg-gray-50 dark:bg-slate-900">
+        <Header />
+        <SidebarToggle
+          toggleSidebarOpen={toggleSidebarOpen}
+          sidebarOpen={sidebarOpen}
+        />
+        <Sidebar isOpen={sidebarOpen} menuRef={menuRef} />
+        <SomePage />
+      </div>
+    </>
   );
 }
 
